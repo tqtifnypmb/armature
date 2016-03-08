ca162bc3dfede046e5ecde4b3c15393b114be936
tqti <tqtifnypmb@gmail.com>
2016-03-08 11:26:49 +0800

    make modules works

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "Armature" ,
+    exclude: [] ,
     targets: [
         Target(
             name: "Armature" ,
@@ -9,3 +10,10 @@ let package = Package(
         )
     ]
 )
+
+//with the new swiftpm we have to force it to create a static lib so that we can use it
+//from xcode. this will become unnecessary once official xcode+swiftpm support is done.
+//watch progress: https://github.com/apple/swift-package-manager/compare/xcodeproj?expand=1
+
+//let lib = Product(name: "Armature", type: .Library(.Dynamic), modules: "Armature")
+//products.append(lib)
